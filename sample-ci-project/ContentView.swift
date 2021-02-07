8ba357a34646078e8b334df7d547685708ba970a
ckincal <can.kincal@hepsiburada.com>
2021-02-07 14:56:06 +0300

    Initial Commit

---
+++ sample-ci-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sample-ci-project
+//
+//  Created by Can Kıncal on 7.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sample-ci-project/sample_ci_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_ci_projectApp.swift
+//  sample-ci-project
+//
+//  Created by Can Kıncal on 7.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct sample_ci_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
