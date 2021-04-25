15daead9aaef98651568db49fbeb154d20a2c7a7
ckincal <can.kincal@hepsiburada.com>
2021-04-25 17:19:30 +0300

    test commit

--- sample-ci-projectUITests/sample_ci_projectUITests.swift
+++ sample-ci-projectUITests/sample_ci_projectUITests.swift
@@ -28,6 +28,8 @@ class sample_ci_projectUITests: XCTestCase {
         app.launch()
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Commit
+
     }
 
     func testLaunchPerformance() throws {
